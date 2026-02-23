d459afd754af52b2f375e8cea992cae201c4c739
ayush-h1 <146382093+ayush-h1@users.noreply.github.com>
2026-02-24 00:58:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicLayout from './components/PublicLayout';
 import PrivateRoute from './components/PrivateRoute';
+import AdminRoute from './components/AdminRoute';
 import Home from './pages/Home';
-import Login from './pages/Login';
+import Login from './pages/Login';          // ← make sure this import exists
 import Register from './pages/Register';
 import Leaderboard from './pages/Leaderboard';
 import Dashboard from './pages/Dashboard';
@@ -15,7 +16,6 @@ import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import Payment from './pages/Payment';
 import TopUp from './pages/TopUp';
-import AdminRoute from './components/AdminRoutes';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import AdminUsers from './pages/admin/AdminUsers';
 import AdminInvestments from './pages/admin/AdminInvestments';
@@ -30,13 +30,12 @@ function App() {
         {/* Public routes */}
         <Route element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />            {/* ← add this */}
           <Route path="/register" element={<Register />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
-          
         </Route>
 
-        {/* Protected routes */}
+        {/* Protected user routes */}
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         <Route path="/invest" element={<PrivateRoute><Invest /></PrivateRoute>} />
         <Route path="/my-investments" element={<PrivateRoute><MyInvestments /></PrivateRoute>} />
@@ -49,14 +48,12 @@ function App() {
         <Route path="/topup" element={<PrivateRoute><TopUp /></PrivateRoute>} />
 
         {/* Admin routes */}
-       <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
-       <Route path="/admin/users" element={<AdminRoute><AdminUsers /></AdminRoute>} />
-       <Route path="/admin/investments" element={<AdminRoute><AdminInvestments /></AdminRoute>} />
-       <Route path="/admin/withdrawals" element={<AdminRoute><AdminWithdrawals /></AdminRoute>} />
-       <Route path="/admin/packages" element={<AdminRoute><AdminPackages /></AdminRoute>} />
-       <Route path="/admin/payments" element={<AdminRoute><AdminPayments /></AdminRoute>} />
-        
-        
+        <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+        <Route path="/admin/users" element={<AdminRoute><AdminUsers /></AdminRoute>} />
+        <Route path="/admin/investments" element={<AdminRoute><AdminInvestments /></AdminRoute>} />
+        <Route path="/admin/withdrawals" element={<AdminRoute><AdminWithdrawals /></AdminRoute>} />
+        <Route path="/admin/packages" element={<AdminRoute><AdminPackages /></AdminRoute>} />
+        <Route path="/admin/payments" element={<AdminRoute><AdminPayments /></AdminRoute>} />
       </Routes>
     </BrowserRouter>
   );
